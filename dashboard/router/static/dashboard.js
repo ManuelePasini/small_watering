ed80e99f2d082533729d197e99d575a154d144a4
Manuele Pasini <manuele.pasini@studio.unibo.it>
2024-09-16 16:39:30 +0200

    feat: added timeout on pump on

--- dashboard/router/static/dashboard.js
+++ dashboard/router/static/dashboard.js
@@ -116,7 +116,7 @@ $(document).ready(function () {
     $('#togglePump').click(function () {
         fetch('/pump/', { method: 'POST' }).then(response => response.json()).then(data => {
             $('#pumpStatus').text(data.pump_state);
-
+            console.log(data.pump_state)
             if (data.pump_state === true) {
                 $('#togglePump').prop('disabled', true);
     
