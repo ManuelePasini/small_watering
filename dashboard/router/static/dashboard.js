e9b70656f0029114d129cc09e099bd2cf41f36b0
Manuele Pasini <manuele.pasini@studio.unibo.it>
2024-09-17 17:04:27 +0200

    testing stuff

--- dashboard/router/static/dashboard.js
+++ dashboard/router/static/dashboard.js
@@ -198,17 +198,22 @@ $(document).ready(async function () {
             $('#togglePump').prop('disabled', true);
             $('#chooseOptimal').prop('disabled', false);
             $('#pumpMode').text('Automatico');
-            selectedOptimal = get_optimal_from_name("Slider");
-            upsertIrrigationControls(selectedOptimal);
-            fetch('/irrigation/mode?mode=slider', { method: 'POST' })
+            //selectedOptimal = get_optimal_from_name("Slider");
+            //upsertIrrigationControls(selectedOptimal);
+            if(selectedOptimal.name == 'Slider'){
+                fetch('/irrigation/mode?mode=slider', { method: 'POST' })
+            }else{
+                fetch('/irrigation/mode?mode=matrix', { method: 'POST' })
+            }
+            
             fetch('/irrigation/slider?value=' + getLastOptimalMoistureValue(), { method: 'POST' });
         } else if (pumpMode == PumpMode.Auto) {
             pumpMode = PumpMode.Manual;
             $('#togglePump').prop('disabled', false);
             $('#chooseOptimal').prop('disabled', false);
             $('#pumpMode').text('Manuale');
-            selectedOptimal = get_optimal_from_name("Slider");
-            upsertIrrigationControls(selectedOptimal);
+            //selectedOptimal = get_optimal_from_name("Slider");
+            //upsertIrrigationControls(selectedOptimal);
             fetch('/irrigation/mode?mode=manual', { method: 'POST' })
         }
     });
