3a5a17261837794fd1457666c3f0d8f20ddbb4c7
Manuele Pasini <manuele.pasini@studio.unibo.it>
2024-09-24 10:25:18 +0200

    feat: fixing optimal update

--- dashboard/router/static/dashboard.js
+++ dashboard/router/static/dashboard.js
@@ -85,8 +85,8 @@ $(document).ready(async function () {
             if (pumpMode == PumpMode.Auto){
                 if (target.id.startsWith('slider')) {
                     fetch('/irrigation/mode?mode=slider', { method: 'POST' })
-                    fetch('/irrigation/slider?value=' + getLastNormalizedOptimalMoistureValue(), { method: 'POST' });
-                    updateSliderValue(getLastNormalizedOptimalMoistureValue())
+                    fetch('/irrigation/slider?value=' + getNormalizedLastOptimalMoistureValue(), { method: 'POST' });
+                    updateSliderValue(getNormalizedLastOptimalMoistureValue())
                 } else {
                     fetch('/irrigation/mode?mode=matrix', { method: 'POST' });
                     updateControlMatrixValues(selectedOptimal.value.data, true);
@@ -94,8 +94,8 @@ $(document).ready(async function () {
             } else {
                 if (target.id.startsWith('slider')) {
                     fetch('/irrigation/mode?mode=manual-slider', { method: 'POST' });
-                    fetch('/irrigation/slider?value=' + getLastNormalizedOptimalMoistureValue(), { method: 'POST' }); 
-                    updateSliderValue(getLastNormalizedOptimalMoistureValue())
+                    fetch('/irrigation/slider?value=' + getNormalizedLastOptimalMoistureValue(), { method: 'POST' }); 
+                    updateSliderValue(getNormalizedLastOptimalMoistureValue())
                 }else{
                     fetch('/irrigation/mode?mode=manual-matrix', { method: 'POST' });
                     await updateControlMatrixValues(selectedOptimal.value.data, true);
